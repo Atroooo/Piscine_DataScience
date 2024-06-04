bd3549ce836e368c08a1820647cf19efdfc52e52
Lucas Compiegne <lucas.compiegne1@gmail.com>
2024-06-04 15:27:56 +0200

    need to fix

--- python_0_starting/ex03/NULL_not_found.py
+++ python_0_starting/ex03/NULL_not_found.py
@@ -5,16 +5,24 @@ def NULL_not_found(object: any) -> int:
         '': "Empty",
         False: "Fake",
     }
-    type_name = object_list.get(object, "Type not Found")
+    type_name = object_list.get(object, "Type Not Found")
     if type(object) is float and object != object:
         print(f"Cheese: {object} {type(object)}")
         return 0
     elif object == 0 and type(object) is int:
         print(f"Zero: {object} {type(object)}")
         return 0
-    elif type_name == "Type not Found":
+    elif type_name == "Type Not Found":
         print(type_name)
         return 1
     else:
         print(f"{type_name}: {object} {type(object)}")
     return 0
+
+# Not working properly
+# >>> NULL_not_found(0.0)
+# Fake: 0.0 <class 'float'>
+# >>> NULL_not_found("")
+# Empty:  <class 'str'>
+# >>> NULL_not_found("0")
+# Zero: 0 <class 'str'>
