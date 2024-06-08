cd72a93ce0118b97387a385292420f9740eb3c9d
Lucas Compiegne <lucas.compiegne1@gmail.com>
2024-06-08 17:46:45 +0200

    backup but too slow

--- datascience_1/ex01/customers_table.py
+++ datascience_1/ex01/customers_table.py
@@ -1,50 +1,68 @@
-import os
 import pandas as pd
 import sqlalchemy
 
 
-def get_table_from_db(table_name: str) -> pd.DataFrame:
-    #Get les tables SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';
-    engine = sqlalchemy.create_engine(
-            "postgresql://lcompieg:mysecretpassword@localhost:5432/piscineds")
+def get_table_from_db(engine: object, table_name: str) -> pd.DataFrame:
     return pd.read_sql_query(f'select * from {table_name}', con=engine)
 
 
-def get_all_files(path: str) -> list:
-    files = os.listdir(path)
-    for file in files:
-        table_name = file.split('.')
-        assert len(table_name) == 2, "Error in file name"
-    return files
+def get_all_table(connection: object, name: str) -> list:
+    sql = sqlalchemy.text(
+        'SELECT * FROM pg_catalog.pg_tables WHERE schemaname != \'pg_catalog\'\
+        AND schemaname != \'information_schema\'')
+    sql_result = connection.execute(sql)
+    results_as_dict = sql_result.mappings().all()
+    table_list = []
+    for key in results_as_dict:
+        table_list.append(key["tablename"])
+    for table in table_list:
+        if not table.startswith(name):
+            table_list.remove(table)
+    return table_list
 
 
-def join_df(files: list, path: str) -> pd.DataFrame:
-    assert isinstance(files, list) or isinstance(path, str), \
-        "join_df take list and str as parameters"
+def create_table(engine: object, data: pd.DataFrame) -> None:
+    data_types = {
+            "event_time": sqlalchemy.DateTime(),
+            "event_type": sqlalchemy.types.String(length=255),
+            "product_id": sqlalchemy.types.Integer(),
+            "price": sqlalchemy.types.Float(),
+            "user_id": sqlalchemy.types.BigInteger(),
+            "user_session": sqlalchemy.types.UUID(as_uuid=True)
+        }
+    data.to_sql("customers", engine, index=False, dtype=data_types,
+                if_exists='replace')
+
+
+def join_df(engine: object, tables: list) -> pd.DataFrame:
+    assert isinstance(tables, list), "join_df take list as parameter"
     final_df = pd.DataFrame()
-    for file in files:
+    for table in tables:
         try:
-            # temp_df = load(path + str(file))
+            temp_df = get_table_from_db(engine, table)
             final_df = pd.concat([final_df, temp_df])
         except Exception as e:
             print(e)
             return None
+    print("Before sort")
     final_df = final_df.sort_values(by=['event_time'])
+    print("Final dataFrame created")
+    create_table(engine, final_df)
     return final_df
 
 
 def main():
     """Main function to test the functions.
     """
-    path = "/home/lcompieg/sgoinfre/custom/"
     try:
-        # files = get_all_files(path)
-        print(get_table_from_db("data_2022_oct"))
-        # df = join_df(files, path)
+        engine = sqlalchemy.create_engine(
+            "postgresql://lcompieg:mysecretpassword@localhost:5432/piscineds")
+        connection = engine.connect()
+        tables = get_all_table(connection, "data_20")
+        df = join_df(engine, tables)
     except Exception as e:
         print(e)
         return
-    # print(df.head(-5))
 
 
 if __name__ == "__main__":
