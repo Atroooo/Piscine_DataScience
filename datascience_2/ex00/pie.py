0b25a17d056dcc8b91ec906671617eec6023d22c
Lucas Compiegne <lucas.compiegne1@gmail.com>
2024-06-10 17:22:12 +0200

    init data2

---
+++ datascience_2/ex00/pie.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+
+
+
+def main():
+    """Main function to test the functions.
+    """
+    engine = sqlalchemy.create_engine(
+            "postgresql://lcompieg:mysecretpassword@localhost:5432/piscineds")
+    data = pd.read_sql_query(f'select * from customers', con=engine)
+
+
+if __name__ == "__main__":
+    main()
