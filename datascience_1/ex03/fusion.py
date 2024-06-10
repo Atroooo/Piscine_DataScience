494a577bfdd94f744b288235fdd509704cb89208
Lucas Compiegne <lucas.compiegne1@gmail.com>
2024-06-10 14:53:55 +0200

    requirements + fusion might be working

--- datascience_1/ex03/fusion.py
+++ datascience_1/ex03/fusion.py
@@ -8,6 +8,23 @@ def main():
         engine = sqlalchemy.create_engine(
             "postgresql://lcompieg:mysecretpassword@localhost:5432/piscineds")
         connection = engine.connect()
+        sql = sqlalchemy.text(
+            'SELECT * \
+            FROM customers \
+            ORDER BY event_time DESC; \
+            ALTER TABLE customers \
+            RENAME TO customers_base;'
+        )
+        connection.execute(sql)
+        sql = sqlalchemy.text(
+            'SELECT * \
+            INTO customers \
+            FROM item \
+            FULL OUTER JOIN customers_base \
+            ON customers_base.product_id = item.product_id;'
+        )
+        connection.execute(sql)
+        engine.dispose()
     except Exception as e:
         print(e)
 
